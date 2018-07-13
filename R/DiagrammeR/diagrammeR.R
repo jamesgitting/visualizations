468dec79be47df27a702dbcd4a5ff157f568bb0b
James Gitting <james.gitting@gmail.com>
2018-07-13 10:27:27 -0700

    Adding a simple DiagrammeR graph

---
+++ R/DiagrammeR/diagrammeR.R
@@ -0,0 +1,34 @@
+# DiagrammeR Example ----------------------------------------------------------------------------
+# Sunburst charts can be a good visualization for sequential data.
+# One scenario is identifying user navigation of a website.
+# Using clickstream data, we can visualize paths users are taking and perhaps identifying
+# areas to improve our UI
+
+
+# Libraries -----------------------------------------------------------------------------------
+library(DiagrammeR)
+library(DiagrammeRsvg)
+
+
+# Configuration -------------------------------------------------------------------------------
+output_dir <- file.path("./DiagrammeR/visuals/")
+
+
+
+# Samples -------------------------------------------------------------------------------------
+
+simple_gv <- grViz("
+
+digraph boxes_and_circles {
+
+  node [shape = circle]
+  A; B; C; D; E; F
+
+  node [shape = triangle]
+  1; 2; 3; 4; 5; 6; 7; 8
+
+  # Edge Statements
+  A->1; B->2; B->3;
+}
+
+")
