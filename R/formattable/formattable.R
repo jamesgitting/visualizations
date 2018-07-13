45791600168dea77f3c2988cba9bd783d60e297d
James Gitting <james.gitting@gmail.com>
2018-07-13 10:50:28 -0700

    Adding formattable example

---
+++ R/formattable/formattable.R
@@ -0,0 +1,28 @@
+# formattable Example ----------------------------------------------------------------------------
+# <Insert Short Description and Use Cases>
+
+
+# Libraries -----------------------------------------------------------------------------------
+library(formattable)
+library(tidyverse)
+library(babynames)
+
+# Configuration -------------------------------------------------------------------------------
+
+
+
+# Sample --------------------------------------------------------------------------------------
+
+set.seed(2018)
+
+sample_data <- babynames %>% 
+  sample_n(20, replace = FALSE) %>%
+  mutate(age = as.integer(format(Sys.Date(), "%Y")) - year) %>%
+  rowid_to_column("id") %>%
+  select(id, name, sex, age)
+
+formattable(sample_data,
+            list(
+             age = color_tile("yellow", "red"),
+             sex = formatter("span", style = x ~ style(color = ifelse(x == "F", "red", "blue")))
+            ))
